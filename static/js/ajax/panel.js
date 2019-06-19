df3c79d7c9a6634b938c76ae006e2a9b59a400e5
Vsevolod Ovchinnikov <sevavov.2004@gmail.com>
2019-06-19 13:24:32 +0300

    Added ajax func and manu restyle

--- static/js/ajax/panel.js
+++ static/js/ajax/panel.js
@@ -5,6 +5,28 @@ function refresh()
     window.location.href = sURL;
 }
 
+function show_menu() {
+    let menu = document.getElementById("menu");
+    let menu_open = document.getElementById("menu_open");
+    let menu_close = document.getElementById("menu_close");
+    let main = document.getElementById("main");
+    menu.style.display = 'block';
+    menu_open.style.display = 'none';
+    menu_close.style.display = 'block';
+}
+
+
+function close_menu(){
+    let menu = document.getElementById("menu");
+    let menu_open = document.getElementById("menu_open");
+    let menu_close = document.getElementById("menu_close");
+    let main = document.getElementById("main");
+    menu.style.display = 'none';
+    menu_open.style.display = 'block';
+    menu_close.style.display = 'none';
+}
+
+
 function send_room_light(on, lamp_id) {
     let status = document.getElementById("status_"+lamp_id);
     let statusoff = document.getElementById("statusoff_"+lamp_id);
@@ -40,8 +62,94 @@ function send_room_light(on, lamp_id) {
 
 function delete_room(room_id) {
     let room = document.getElementById("room_"+room_id);
-    $.get("/edit/room/"+ room_id + "/delete/");
-    room.style.display = "none";
+    $.ajax({
+        url: '/add/' + room_id.toString() + '/room/',
+        type: 'GET',
+        success: function(result) {
+            show_notification("Комната будет окончательно удалена сразу после перезагрузки страницы.", 'success', 'bottom-right');
+            setTimeout(refresh, 3000);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if(jqXHR.status === 404 || errorThrown === 'Not Found')
+            {
+                show_notification("Данной комнаты не существует.\n Перезагрузите страницу.", 'error', 'bottom-right');
+                setTimeout(refresh, 3000);
+            }
+            else if(jqXHR.status === 403){
+                show_notification("Доступ запрещён.", 'error', 'bottom-right');
+            }
+        }
+    });
+}
+
+
+function add_room(home_id) {
+    let send = {};
+    let input1 = document.getElementById("input2_1");
+    let input2 = document.getElementById("input2_2");
+    send.name = input1.value;
+    send.key = input2.value;
+    let csrftoken = getCookie('csrftoken');
+    $.ajax({
+        beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        },
+        url: '/add/' + home_id.toString() + '/room/',
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(send),
+        dataType: 'text',
+        success: function(result) {
+            show_notification("Комната будет добавлена на панель сразу после перезагрузки страницы.", 'success', 'bottom-right');
+            setTimeout(refresh, 3000);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if(jqXHR.status === 404 || errorThrown === 'Not Found')
+            {
+                show_notification("Внутренняя ошибка сервера.\n Перезагрузите страницу.", 'error', 'bottom-right');
+                setTimeout(refresh, 3000);
+            }
+            else if(jqXHR.status === 403){
+                show_notification("Неверный Ключ Активации.\n Повторите попытку.", 'error', 'bottom-right');
+            }
+        }
+    });
+}
+
+
+function edit_house(home_id) {
+    let send = {};
+    let input1 = document.getElementById("input1_1");
+    send.name = input1.value;
+    let csrftoken = getCookie('csrftoken');
+    $.ajax({
+        beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        },
+        url: '/edit/' + home_id.toString() + '/',
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(send),
+        dataType: 'text',
+        success: function(result) {
+            show_notification("Изменения вступят в силу сразу после перезагрузки страницы.", 'success', 'bottom-right');
+            setTimeout(refresh, 3000);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if(jqXHR.status === 404 || errorThrown === 'Not Found')
+            {
+                show_notification("Дом не найден.\n Перезагрузите страницу.", 'error', 'bottom-right');
+                setTimeout(refresh, 3000);
+            }
+            else if(jqXHR.status === 403){
+                show_notification("Доступ запрещён.", 'error', 'bottom-right');
+            }
+        }
+    });
 }
 
 
@@ -62,16 +170,17 @@ function edit_room(room_id) {
         data: JSON.stringify(send),
         dataType: 'text',
         success: function(result) {
-            show_notification("Изменения вступят в силу после перезагрузки страницы.", 'success', 'bottom-right');
+            show_notification("Изменения вступят в силу сразу после перезагрузки страницы.", 'success', 'bottom-right');
             setTimeout(refresh, 3000);
         },
         error: function(jqXHR, textStatus, errorThrown) {
             if(jqXHR.status === 404 || errorThrown === 'Not Found')
             {
-                show_notification("Комнаты не существует.\n Перезагрузите страницу.")
+                show_notification("Комната не найдена.\n Перезагрузите страницу.", 'error', 'bottom-right');
+                setTimeout(refresh, 3000);
             }
             else if(jqXHR.status === 403){
-                show_notification("Доступ запрещён.")
+                show_notification("Доступ запрещён.", 'error', 'bottom-right');
             }
         }
     });
@@ -84,15 +193,6 @@ function modal_input_2_fields_setup(text, onclick, field1="", label1="", field2=
     modal_label1.innerHTML = label1;
     modal_field1.setAttribute('placeholder', label1);
     modal_field1.setAttribute('value', field1);
-    modal_field1.addEventListener("keyup", function(event) {
-      // Number 13 is the "Enter" key on the keyboard
-      if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("myBtn").click();
-      }
-    });
     let modal_field2 = document.getElementById("input2_2");
     let modal_label2 = document.getElementById("label2_2");
     modal_label2.innerHTML = label2;
